c68d0319f58700f9535edafc5718d12b84e20e1f
FahrulNa <140799150+FahrulNa@users.noreply.github.com>
2023-08-11 15:32:21 +0700

    guess.py

--- guess.py
+++ guess.py
@@ -2,9 +2,8 @@ class  Guess:
     def __init__(self,jawaban,tebakan):
         self.jawaban= " "
         self.tebakan= " "
-
-def cek(tebakan,jawaban):
-    if tebakan == jawaban:
-        return True
-    return False
+    def cek(tebakan,jawaban):
+        if tebakan == jawaban
+            return True
+        return False
     
